1a7de5ff4a41c97e9c37da7f6163d8f7f41afac8
郭冉 <guo.r@belle.com.cn>
2019-05-09 18:41:57 +0800

    up

---
+++ meta.js
@@ -0,0 +1,22 @@
+{
+    prompts: {
+        name: {
+            type: "input",
+            message: "项目名称"
+        },
+        author: {
+            type: "input",
+            message: "作者"
+        },
+        email: {
+            type: "input",
+            message: "邮箱",
+            validate: function(_email){
+                if(/@/g.test(_email)){
+                    return true
+                }
+                return '邮箱不合法'
+            }
+        }
+    }
+}
